2434b3ac9f01d1e53a023f525747e674a3cf417d
hrlnsnchz <hrlnsnchz@gmail.com>
2021-06-04 12:09:12 -0500

    Adds test for deleting a product by its id. Test passed.

--- tests/product.py
+++ tests/product.py
@@ -1,7 +1,9 @@
 import json
 import datetime
+from django.http import response
 from rest_framework import status
 from rest_framework.test import APITestCase
+from bangazonapi.models import Product
 
 
 class ProductTests(APITestCase):
@@ -95,6 +97,18 @@ class ProductTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(json_response), 3)
 
-    # TODO: Delete product
+    def test_delete_product(self):
+        """
+        Ensure we can delete products
+        """
+        self.test_create_product()
+        url="/products/1"
+
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     # TODO: Product can be rated. Assert average rating exists.
